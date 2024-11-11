a4e10f9ed4f29e143b4d006b8b239f73327a5115
ferruccio4 <ferruccio4@gmail.com>
2024-11-11 17:06:20 +0100

    Small tweaks to improve usability of moving platforms

--- scripts/moving_object_path_3d.gd
+++ scripts/moving_object_path_3d.gd
@@ -1,3 +1,4 @@
+@tool
 extends Path3D
 @onready var path_follow_3d: PathFollow3D = $PathFollow3D
 @onready var remote_transform_3d: RemoteTransform3D = $PathFollow3D/RemoteTransform3D
@@ -13,20 +14,30 @@ var direction : float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	remote_transform_3d.remote_path = object_to_move.get_path()
-	object_to_move.set_deferred("position", Vector3.ZERO)
+	if not Engine.is_editor_hint():
+		if object_to_move == null:
+			printerr("You need to set an object to move in ", name)
+			get_tree().quit()
+		else:
+			remote_transform_3d.remote_path = object_to_move.get_path()
+			object_to_move.set_deferred("position", Vector3.ZERO)
+			global_position = Vector3.ZERO
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if object_to_move and is_on_from_start:
-		if ping_pong:
-			if path_follow_3d.progress_ratio > 0.99:
-				direction = -1
-			if path_follow_3d.progress_ratio < 0.01:
-				direction = 1
+	if not Engine.is_editor_hint():
+		if object_to_move and is_on_from_start:
+			if ping_pong:
+				if path_follow_3d.progress_ratio > 0.99:
+					direction = -1
+				if path_follow_3d.progress_ratio < 0.01:
+					direction = 1
 
-		path_follow_3d.progress += (delta*movement_speed*direction)
+			path_follow_3d.progress += (delta*movement_speed*direction)
+
+	if Engine.is_editor_hint() and global_position != Vector3.ZERO:
+		global_position = Vector3.ZERO
 
 
 func receive_input(on : bool) -> void:

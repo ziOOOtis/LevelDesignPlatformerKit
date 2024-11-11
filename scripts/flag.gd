b5a831fb292cd5313713c2fccf3d50d7f49d34a1
ferruccio4 <ferruccio4@gmail.com>
2024-11-11 18:01:03 +0100

    Added speedrun feature and fixed some bugs

--- scripts/flag.gd
+++ scripts/flag.gd
@@ -1,9 +1,18 @@
 extends Node3D
 var touched : bool
+@export var speedrun_mode : bool
+var hud : CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var flags_amount : int = get_tree().get_nodes_in_group("Flag").size()
+	if flags_amount > 1 and speedrun_mode:
+		printerr("You can't have more than one flag if it's a speedrun level")
+		get_tree().quit()
+	
+	if speedrun_mode:
+		hud = get_tree().get_nodes_in_group("HUD")[0]
+		hud.start_speedrun()
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method("touched_goal") and !touched:
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -3,14 +3,42 @@ extends CanvasLayer
 @onready var coins_label: Label = %CoinsLabel
 @onready var play_again_button: Button = %PlayAgainButton
 @onready var environment: WorldEnvironment = %Environment
+@onready var time_label: Label = %TimeLabel
+@onready var time_name_label: Label = %TimeNameLabel
 
 var original_blur : bool
 var original_blur_distance : float
 var original_blur_amount : float
+var speed_run : bool
+var seconds : float
+var pause_timer : bool
 
 func _ready() -> void:
 	level_complete.visible = false
 	play_again_button.visible = false
+	time_label.visible = false
+	time_name_label.visible = false
+
+func _process(delta: float) -> void:
+	if speed_run and not pause_timer:
+		time_label.visible = true
+		time_name_label.visible = true
+		seconds += delta
+		var minutes : String
+		if (seconds / 60) > 1:
+			var minutes_int : int = seconds / 60
+			minutes = str(minutes_int) + " : "
+		else:
+			minutes = ""
+		var leftover_seconds : String
+		if (int(seconds) % 60) < 10:
+			leftover_seconds = "0" + str(int(seconds) % 60)
+		else:
+			leftover_seconds = str(int(seconds) % 60)
+		time_label.text = minutes + leftover_seconds
+
+func start_speedrun():
+	speed_run = true
 
 func _on_coin_collected(coins):
 	get_tree().call_group("Door", "coin_amount_updated",coins)
@@ -26,6 +54,8 @@ func _on_player_reached_goal() -> void:
 	environment.camera_attributes.set("dof_blur_amount", 0.1)
 	level_complete.visible = true
 	play_again_button.visible = true
+	if speed_run:
+		pause_timer = true
 	get_tree().paused = true
 
 

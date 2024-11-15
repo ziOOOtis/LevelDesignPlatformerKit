c303b750df23970f5084ca7289e4d01160d29dbc
ferruccio4 <ferruccio4@gmail.com>
2024-11-15 10:52:11 +0100

    Small tweak to button, added a sample level

--- scripts/button.gd
+++ scripts/button.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var pressed_by_default : bool
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 var button_state : bool = false
+@onready var cooldown_timer: Timer = $CooldownTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,13 +19,15 @@ func _process(_delta: float) -> void:
 	pass
 
 func _on_button_area_3d_area_entered(_area: Area3D) -> void:
-	if not animation_player.is_playing():
-		if button_state:
-			button_state = false
-			animation_player.play("toggle-off")
-		else:
-			button_state = true
-			animation_player.play("toggle-on")
-		
-		if target and target.has_method("receive_input"):
-			target.receive_input(button_state)
+	if cooldown_timer.is_stopped():
+		cooldown_timer.start()
+		if not animation_player.is_playing():
+			if button_state:
+				button_state = false
+				animation_player.play("toggle-off")
+			else:
+				button_state = true
+				animation_player.play("toggle-on")
+			
+			if target and target.has_method("receive_input"):
+				target.receive_input(button_state)

f33406170292abcfcf379cfbd8597ac17a470eda
ziOOOtis <yanlin.liuuu@outlook.com>
2024-12-18 16:21:01 +0100

    almost done

---
+++ scripts/bubbles.gd
@@ -0,0 +1,6 @@
+extends CPUParticles3D
+
+
+# Called when the node enters the scene tree for the first time.
+func receive_input(on : bool) -> void:
+	emitting = true
--- scripts/inWater.gd
+++ scripts/inWater.gd
@@ -1,13 +1,22 @@
 extends Area3D
 
-@onready var aduio_stream:AudioStreamPlayer = $"../AudioStreamPlayer"
-@export var isPlaying : bool = false
+@onready var audio_stream:AudioStreamPlayer = $"../AudioStreamPlayer"
+@export var bubbles: CPUParticles3D
 
-
-	
 
 
 func _on_body_entered(body: Node3D) -> void:
-	isPlaying = true
-	aduio_stream.play()
+	audio_stream.play()
+	bubbles.emitting = true
+	
+
+
+
+
+
+
+func _on_body_exited(body: Node3D) -> void:
+	
+	audio_stream.stop()
+	bubbles.emitting = false
 	

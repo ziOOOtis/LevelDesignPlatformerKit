ee22f324be0133c5a949717ac63d7385221d4db7
ziOOOtis <yanlin.liuuu@outlook.com>
2024-12-20 12:16:55 +0100

    +1

--- scripts/inWater.gd
+++ scripts/inWater.gd
@@ -2,12 +2,14 @@ extends Area3D
 
 @onready var audio_stream:AudioStreamPlayer = $"../AudioStreamPlayer"
 @export var bubbles: CPUParticles3D
+@export var flashLight: SpotLight3D
 
 
 
 func _on_body_entered(body: Node3D) -> void:
 	audio_stream.play()
 	bubbles.emitting = true
+	flashLight.light_energy = 6
 	
 
 
@@ -19,4 +21,6 @@ func _on_body_exited(body: Node3D) -> void:
 	
 	audio_stream.stop()
 	bubbles.emitting = false
+	flashLight.light_energy = 1
+	
 	
